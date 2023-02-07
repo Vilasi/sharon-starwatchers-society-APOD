682a188d7d3a3f641104dde6824c48dae2b08602
Vilasi <VilasiCoding@gmail.com>
2023-02-07 13:53:54 -0500

    Initial html page structure layed out

---
+++ app.js
@@ -0,0 +1,26 @@
+const apiKey = 'duTM1WcUS7QLQrUquX9mICHxV1Vctdh9wwqOVeZE';
+
+//https://api.nasa.gov/planetary/apod?api_key=duTM1WcUS7QLQrUquX9mICHxV1Vctdh9wwqOVeZE
+// {
+//   // `data` is the response that was provided by the server
+//   data: {},
+
+//   // `status` is the HTTP status code from the server response
+//   status: 200,
+
+//   // `statusText` is the HTTP status message from the server response
+//   statusText: 'OK',
+
+//   // `headers` the HTTP headers that the server responded with
+//   // All header names are lowercase and can be accessed using the bracket notation.
+//   // Example: `response.headers['content-type']`
+//   headers: {},
+
+//   // `config` is the config that was provided to `axios` for the request
+//   config: {},
+
+//   // `request` is the request that generated this response
+//   // It is the last ClientRequest instance in node.js (in redirects)
+//   // and an XMLHttpRequest instance in the browser
+//   request: {}
+// }
