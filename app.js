43f8fd92ace4161f7d04dcb50c796f37fe5deb2e
Vilasi <VilasiCoding@gmail.com>
2023-02-07 14:34:53 -0500

    Initial API call functional

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
-const apiKey = 'duTM1WcUS7QLQrUquX9mICHxV1Vctdh9wwqOVeZE';
+// const form = document.querySelector('form');
+// console.log(form.classList);
+// console.log(window.innerWidth);
 
-//https://api.nasa.gov/planetary/apod?api_key=duTM1WcUS7QLQrUquX9mICHxV1Vctdh9wwqOVeZE
-// {
-//   // `data` is the response that was provided by the server
-//   data: {},
+// let screenWidth = window.innerWidth;
 
-//   // `status` is the HTTP status code from the server response
-//   status: 200,
+// function resizeFormInput(width) {
+//   switch (width) {
+//     case width < 992:
+//       form.classList.add('w-100');
+//       break;
 
-//   // `statusText` is the HTTP status message from the server response
-//   statusText: 'OK',
-
-//   // `headers` the HTTP headers that the server responded with
-//   // All header names are lowercase and can be accessed using the bracket notation.
-//   // Example: `response.headers['content-type']`
-//   headers: {},
-
-//   // `config` is the config that was provided to `axios` for the request
-//   config: {},
-
-//   // `request` is the request that generated this response
-//   // It is the last ClientRequest instance in node.js (in redirects)
-//   // and an XMLHttpRequest instance in the browser
-//   request: {}
+//     default:
+//       break;
+//   }
 // }
