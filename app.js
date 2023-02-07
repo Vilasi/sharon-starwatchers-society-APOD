3cc0cd4d5b429588bcb0e1083d2b62f7acb40b78
Vilasi <VilasiCoding@gmail.com>
2023-02-07 18:58:28 -0500

    Refactored functions to dry out repeated code functionality in app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const apiKey = 'duTM1WcUS7QLQrUquX9mICHxV1Vctdh9wwqOVeZE';
-const form = document.querySelector('form');
 const dateInput = document.querySelector('#date');
+const submitButtons = document.querySelectorAll('.submitButton');
 
+// Call the NASA API
 async function apiCall(date) {
   const options = {
     baseURL: 'https://api.nasa.gov/',
@@ -13,28 +14,14 @@ async function apiCall(date) {
     },
   };
 
-  const response = await axios(options);
-  return response;
+  return await axios(options);
 }
 
-function buildDOM(response) {
-  const image = document.querySelector('#img');
-  const description = document.querySelector('#description');
-  const title = document.querySelector('#title');
-
-  image.src = response.data.hdurl;
-  description.innerText = response.data.explanation;
-  title.innerText = response.data.title;
-}
-
-function handleError(error) {
-  const description = document.querySelector('#description');
-  description.innerText = "Sorry, we don't have an image for this request";
-}
-
+// Try/Catch API and call DOM rendering function if successful.
 async function tryAPI(date) {
   try {
     const response = await apiCall(date);
+    console.log(response);
     buildDOM(response);
   } catch (error) {
     handleError(error);
@@ -42,29 +29,66 @@ async function tryAPI(date) {
   }
 }
 
-function formatDate(date, monthDayYear = false) {
+// Build DOM with API Success Response
+function buildDOM(response) {
+  const image = document.querySelector('#img');
+  const video = document.querySelector('#video');
+  const videoDiv = document.querySelector('#video-div');
+  const description = document.querySelector('#description');
+  const title = document.querySelector('#title');
+
+  if (response.data.media_type === 'image') {
+    image.src = response.data.hdurl;
+    videoDiv.classList.add('d-none');
+    image.classList.remove('d-none');
+  } else if (response.data.media_type === 'video') {
+    video.src = response.data.url;
+    videoDiv.classList.remove('d-none');
+    image.classList.add('d-none');
+  }
+
+  description.innerText = response.data.explanation;
+  title.innerText = response.data.title;
+}
+
+// Build DOM with API Failure Response
+function handleError(error) {
+  const description = document.querySelector('#description');
+
+  description.innerText = "Sorry, we don't have an image for this request";
+}
+
+// Make a properly formatted date string out of a new Date() argument
+function formatDate(date) {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
 
-  //   if (monthDayYear) {
-  //     return `${month}-${day}-${year}`;
-  //   }
   return `${year}-${month}-${day}`;
 }
 
-// function formtDateMonthDatYear
+function getTodaysDate() {
+  const date = new Date();
 
-form.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  await tryAPI(dateInput.value);
-});
+  return formatDate(date);
+}
+
+// Call API function chain based on ID of form button clicked.
+for (const button of submitButtons) {
+  button.addEventListener('click', async (e) => {
+    const id = e.target.id;
+
+    if (id === 'submit') {
+      await tryAPI(dateInput.value);
+    } else if (id === 'jump-to-today') {
+      const todaysDate = getTodaysDate();
+      await tryAPI(todaysDate);
+    }
+  });
+}
 
 window.addEventListener('load', async () => {
-  const date = new Date();
-  const todaysDate = formatDate(date);
-  //   const input = document.querySelector('#date');
-  //   input.placeholder = formatDate(date, true);
+  const todaysDate = getTodaysDate();
 
   await tryAPI(todaysDate);
 });
